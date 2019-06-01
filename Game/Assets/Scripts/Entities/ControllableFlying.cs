39194b2055b48e24bb9ceeb4354dde99d5151b34
Juutis <jutkula@gmail.com>
2019-06-01 13:59:29 +0300

    Fixed flying

--- Game/Assets/Scripts/Entities/ControllableFlying.cs
+++ Game/Assets/Scripts/Entities/ControllableFlying.cs
@@ -8,7 +8,6 @@ public class ControllableFlying : MonoBehaviour
     GameObject childSprite;
     [SerializeField]
     Rigidbody2D body;
-    Vector2 velocity;
     float direction;
     [SerializeField]
     float lift = 1f;
@@ -16,14 +15,15 @@ public class ControllableFlying : MonoBehaviour
     float rotateSpeed = 1f;
     [SerializeField]
     float maxVel;
-    float speed = 0f;
+    float speed = 5f;
     float maxSpeed = 20f;
     float gravity = 0f;
     Quaternion startRot;
     float startTime;
     float dropAcc = -1;
     float damp = 0.99f;
-    bool accelerating;
+
+    bool firstTick = true;
 
     // Start is called before the first frame update
     void Start()
@@ -34,9 +34,10 @@ public class ControllableFlying : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (body.velocity.magnitude > 0.1f)
+        if (body.velocity.magnitude > 0.01f)
         {
-            transform.rotation = Quaternion.LookRotation(body.velocity, Vector3.up);
+            float angleDiff = Vector3.SignedAngle(transform.right, body.velocity, Vector3.forward);
+            transform.Rotate(Vector3.forward, angleDiff);
         }
         Debug.DrawLine(transform.position, transform.position + Vector3.down, Color.red);
         Debug.DrawLine(transform.position, transform.position + new Vector3(body.velocity.x, body.velocity.y, 0));
@@ -44,6 +45,12 @@ public class ControllableFlying : MonoBehaviour
 
     private void FixedUpdate()
     {
+        if (firstTick)
+        {
+            firstTick = false;
+            body.velocity = Vector3.right * speed;
+        }
+
         float gravityAcc = 9.81f / 2 * Time.fixedDeltaTime;
         if (speed > 5f)
         {
@@ -55,23 +62,11 @@ public class ControllableFlying : MonoBehaviour
         }
 
         gravity = gravity * Mathf.Max(0, (10 - speed) / 10);
-
-        Vector2 v2 = body.velocity;
-        if(v2.magnitude < 0.1f)
-        {
-            v2 = transform.forward.normalized;
-        }
-
-        velocity = v2.normalized * speed;
-        body.velocity = velocity;
+        
+        body.velocity = body.velocity.normalized * speed;
         body.velocity += Vector2.down * gravity;
-        if (!accelerating)
-        {
-            //dampening
-            //speed = speed - (speed * 1.5f * Time.fixedDeltaTime);
-        }
-        Debug.Log(speed + ", " + body.velocity);
-        accelerating = false;
+
+
     }
 
     public void Accelerate()
@@ -79,7 +74,6 @@ public class ControllableFlying : MonoBehaviour
         Debug.Log("Accelerate");
         speed += 4f * Time.fixedDeltaTime;
         speed = Mathf.Min(speed, maxSpeed);
-        accelerating = true;
     }
 
     public void Decelerate()
