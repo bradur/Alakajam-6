507dfd01411a70aac922e591c0f9828390e89313
Juutis <jutkula@gmail.com>
2019-06-01 13:09:18 +0300

    TRIP LANE

---
+++ Game/Assets/Scripts/Entities/Triplane.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Triplane : MonoBehaviour
+{
+    [SerializeField]
+    bool TriggerRoll = false;
+
+    bool rolled = false;
+    Animator anim;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (TriggerRoll)
+        {
+            Roll();
+            TriggerRoll = false;
+        }
+    }
+
+    public void Roll()
+    {
+        if (rolled)
+        {
+            anim.SetBool("rolled", false);
+        }
+        else
+        {
+            anim.SetBool("rolled", true);
+        }
+        rolled = !rolled;
+    }
+}
